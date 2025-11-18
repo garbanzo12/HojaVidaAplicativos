5f86ed78eda4f0c7921ff52016069c7349cf9415
Meloncito17 <yuranimelisa10@gmail.com>
2025-11-18 08:46:46 -0500

    tabla de usuarios y login
    formulario del usuario y el login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Dashboard from "./pages/dashboard.js";
+import Login from "./pages/login.js";
 
 function App() {
   return <Dashboard />;
+  return <Login/>;
 }
 
 export default App;
+  
